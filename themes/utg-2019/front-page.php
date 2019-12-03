072977dddf09167fe14b3a6e74ea5258f93cc964
REDiowave <boschdoor.ian@gmail.com>
2019-12-03 15:32:23 -0800

    add front page template

---
+++ themes/utg-2019/front-page.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * The template for displaying all pages.
+ *
+ * @package utg_Theme
+ */
+
+get_header(); ?>
+
+<div id="primary" class="content-area">
+	<div class="home-hero">
+		<div class="home-hero-img"><?php the_post_thumbnail(); ?></div>
+	<main id="main" class="site-main" role="main">
+
+		<?php while (have_posts()) : the_post(); ?>
+
+			<?php get_template_part('template-parts/content', 'page'); ?>
+
+		<?php endwhile; // End of the loop. 
+		?>
+
+	</main><!-- #main -->
+</div><!-- #primary -->
+
+<?php get_footer(); ?>
