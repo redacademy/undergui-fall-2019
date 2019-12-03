4a56d412aa43db054efeec17941a4765b27e9fdf
Sina <sina202@gmail.com>
2019-12-03 11:47:51 -0800

    update footer styling, improve social media icons

--- themes/utg-2019/footer.php
+++ themes/utg-2019/footer.php
@@ -9,22 +9,20 @@
 		</div><!-- #content -->
 
 		<footer id="site-footer" class="site-footer">
-
-							<!-- contact-footer -->
-			<div class="socialmedia-footer">
-				<ul>
-					<li>
-						<a href="#"><img src="./wp-content/themes/utg-2019/assets/icons/facebook.svg" alt="facebook-logo"></a>
-						<a href="#"><img src="./wp-content/themes/utg-2019/assets/icons/instagram.svg" alt="instagram-logo"></a>
-						<a href="#"><img src="./wp-content/themes/utg-2019/assets/icons/twitter.svg" alt="twitter-logo"></a>
-						<a href="#"><img src="./wp-content/themes/utg-2019/assets/icons/youtube.svg" alt="youtube-logo"></a>
-					</li>
-				</ul>
-			</div>
 			
 			<div class="copyright-info">
 				<?php printf( esc_html('© 2020 Under The GUI')); ?></a>
 			</div>
+
+			<div class="socialmedia-footer">
+				<ul>
+					<li><a href="#"><img src="./wp-content/themes/utg-2019/assets/icons/facebook.svg" alt="facebook-logo"></a></li>
+					<li><a href="#"><img src="./wp-content/themes/utg-2019/assets/icons/instagram.svg" alt="instagram-logo"></a></li>
+					<li><a href="#"><img src="./wp-content/themes/utg-2019/assets/icons/twitter.svg" alt="twitter-logo"></a></li>
+					<li><a href="#"><img src="./wp-content/themes/utg-2019/assets/icons/youtube.svg" alt="youtube-logo"></a></li>
+				</ul>
+			</div>
+
 		</footer><!-- #colophon -->
 		</div><!-- #page -->
 
