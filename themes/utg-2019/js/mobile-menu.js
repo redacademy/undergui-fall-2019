d1ab3733b79f26fed22ffa336b2a288e40ebcc96
Kieran Devlin <kjrdevlin@gmail.com>
2019-12-04 15:17:18 -0800

    add button and funcitonality to open/close mobile menu

--- themes/utg-2019/js/mobile-menu.js
+++ themes/utg-2019/js/mobile-menu.js
@@ -1,10 +1,23 @@
 const mobileMenu = document.querySelector('.mobile-menu');
 const navLinks = document.querySelector('.nav-links');
 const siteHeader = document.querySelector('.site-header');
+const closeMenuButton = document.querySelector('.left-arrow');
+const switchArrowDireciton = document.querySelector('.right-arrow');
 
 mobileMenu.addEventListener('click', function(e) {
   e.preventDefault();
 
+  switchArrowDireciton.classList.remove('switch');
   siteHeader.classList.toggle('toggle-menu');
   navLinks.classList.toggle('toggle-menu');
+  navLinks.classList.toggle('open-menu');
+});
+
+closeMenuButton.addEventListener('click', function(e) {
+  e.preventDefault();
+  //   if (switchArrowDireciton) {
+
+  //   }
+  closeMenuButton.classList.toggle('close-menu');
+  switchArrowDireciton.classList.add('switch');
 });
