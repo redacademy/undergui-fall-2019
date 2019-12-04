e6dca1b1be5e94fd59fafc7378878592f16abe5d
REDiowave <boschdoor.ian@gmail.com>
2019-12-04 11:27:51 -0800

    moblie first hero styling

--- themes/utg-2019/front-page.php
+++ themes/utg-2019/front-page.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The template for displaying all pages.
  *
@@ -10,10 +11,11 @@ get_header(); ?>
 <div id="primary" class="content-area">
 	<div class="home-hero-banner">
 		<?php the_post_thumbnail(); ?>
-		<h1 class="home-hero-text">Coders will be the wizards of tomorrow</h1>
-		<button>view our programs</button>
+		<div class="home-hero-text">
+			<h1>Coders will be the wizards of tomorrow</h1>
+			<button class="white-btn">view our programs</button>
+		</div>
 	</div>
-</div>
 	<main id="main" class="site-main" role="main">
 
 		<?php while (have_posts()) : the_post(); ?>
