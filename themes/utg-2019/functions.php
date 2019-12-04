7e34408136e5ff51f3739336efe0cdbaf35264b6
Manav Singh <manavjot13@gmail.com>
2019-12-04 09:33:28 -0800

    added partners content and faq content and moved custom post to plugins, page-about.php

--- themes/utg-2019/functions.php
+++ themes/utg-2019/functions.php
@@ -112,4 +112,4 @@ require get_template_directory() . '/inc/template-tags.php';
  */
 require get_template_directory() . '/inc/extras.php';
 
-require get_template_directory() . '/inc/utg_custom_posts.php';
+
