337d095be1889ea4e9097ac69081471875b3e917
Kieran Devlin <kjrdevlin@gmail.com>
2019-12-04 17:00:34 -0800

    add new colour variables, remove animation issue in mobile nav bar

--- themes/utg-2019/js/mobile-menu.js
+++ themes/utg-2019/js/mobile-menu.js
@@ -4,20 +4,21 @@ const siteHeader = document.querySelector('.site-header');
 const closeMenuButton = document.querySelector('.left-arrow');
 const switchArrowDireciton = document.querySelector('.right-arrow');
 
+//on click hamburger icon, opens mobile menu
 mobileMenu.addEventListener('click', function(e) {
   e.preventDefault();
-
+  closeMenuButton.classList.remove('close-menu');
   switchArrowDireciton.classList.remove('switch');
-  siteHeader.classList.toggle('toggle-menu');
-  navLinks.classList.toggle('toggle-menu');
-  navLinks.classList.toggle('open-menu');
+  siteHeader.classList.add('toggle-menu');
+  navLinks.classList.add('toggle-menu');
+  //   navLinks.classList.toggle('open-menu');
 });
 
+//on click arrow button, switches arrow direction and closes menu
 closeMenuButton.addEventListener('click', function(e) {
   e.preventDefault();
-  //   if (switchArrowDireciton) {
-
-  //   }
-  closeMenuButton.classList.toggle('close-menu');
+  siteHeader.classList.remove('toggle-menu');
+  navLinks.classList.remove('toggle-menu');
+  closeMenuButton.classList.add('close-menu');
   switchArrowDireciton.classList.add('switch');
 });
