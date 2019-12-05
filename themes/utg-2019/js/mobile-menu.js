f352c3eb540c66704fccacde33a0c8c8ea4ac47b
Kieran Devlin <kjrdevlin@gmail.com>
2019-12-05 09:16:07 -0800

    update mobile-menu.js to have more semantic variable names

--- themes/utg-2019/js/mobile-menu.js
+++ themes/utg-2019/js/mobile-menu.js
@@ -1,17 +1,16 @@
-const mobileMenu = document.querySelector('.mobile-menu');
+const hamburgerMenu = document.querySelector('.mobile-menu');
 const navLinks = document.querySelector('.nav-links');
 const siteHeader = document.querySelector('.site-header');
 const closeMenuButton = document.querySelector('.left-arrow');
 const switchArrowDireciton = document.querySelector('.right-arrow');
 
 //on click hamburger icon, opens mobile menu
-mobileMenu.addEventListener('click', function(e) {
+hamburgerMenu.addEventListener('click', function(e) {
   e.preventDefault();
   closeMenuButton.classList.remove('close-menu');
   switchArrowDireciton.classList.remove('switch');
   siteHeader.classList.add('toggle-menu');
   navLinks.classList.add('toggle-menu');
-  //   navLinks.classList.toggle('open-menu');
 });
 
 //on click arrow button, switches arrow direction and closes menu
