742fe84ca747f94b4e443055c235af88af86994f
Kieran Devlin <kjrdevlin@gmail.com>
2019-12-05 15:01:07 -0800

    update path to Gotham fonts

--- themes/utg-2019/js/front-page.js
+++ themes/utg-2019/js/front-page.js
@@ -1,12 +1,10 @@
-document.addEventListener('DOMContentLoaded', function () {
-
-      // Flickity Slider JS
-
-      const elem = document.querySelector('#project-slider');
-      const flkty = new Flickity(elem, {
-        // options
-        cellAlign: 'left',
-        wrapAround: true,
-        freeScroll: true
-
-      })});
+document.addEventListener('DOMContentLoaded', function() {
+  // Flickity Slider JS
+  // const elem = document.querySelector('#project-slider');
+  // const flkty = new Flickity(elem, {
+  //   // options
+  //   cellAlign: 'left',
+  //   wrapAround: true,
+  //   freeScroll: true
+  // })
+});
