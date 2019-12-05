54c8454d32d012980f658c15f425391cac798292
Manav Singh <manavjot13@gmail.com>
2019-12-05 09:11:18 -0800

    added the navigation in page-about made template part and custom field

---
+++ themes/utg-2019/template-parts/about-page-navigation.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Template part for displaying faq in about page.
+ *
+ * @package utg_Theme
+ */
+?>
+
+
+<!-- faq loop for about page -->
+<?php if( have_rows('about_page_navigation') ): ?>
+
+	<!-- loop through the rows of data -->
+	<?php while ( have_rows('about_page_navigation') ) : the_row(); ?>
+		<ul>
+			<li><?php the_sub_field('page_one'); ?></p>
+			<li><?php the_sub_field('page_two'); ?></p>
+			<li><?php the_sub_field('page_three'); ?></p>
+			<li><?php the_sub_field('page_four'); ?></p>
+		</ul>
+	<?php endwhile;
+
+	else :
+
+	// no rows found
+
+	endif; ?>
