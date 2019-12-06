f7b5e566f481207bd407cb21fc21926cee041c33
REDiowave <boschdoor.ian@gmail.com>
2019-12-06 11:44:15 -0800

    add layout for fp testimonials

---
+++ themes/utg-2019/template-parts/fp-latest-comm.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * Template part for displaying project showcase content in front-page.php.
+ *
+ * @package utg_Theme
+ */
+
+?>
+
+<div class="latest-events-container">
+
+	<h2 class="latest-events-header">The latest from our community</h2>
+
+	<div class="events-wrapper">
+
+		<div class="event-container">
+			<img src="https://picsum.photos/id/236/300/200" />
+			<h3 class="event-time">August 5, 2019</h3>
+			<h2 class="event-title">HTML 101</h2>
+			<p class="event-location">Kitsilano</p>
+		</div>
+
+		<div class="event-container">
+			<img src="https://picsum.photos/id/235/300/200" />
+			<h3 class="event-time">September 13, 2019</h3>
+			<h2 class="event-title">CSS 101</h2>
+			<p class="event-location">Kitsilano</p>
+		</div>
+
+		<div class="event-container">
+			<img src="https://picsum.photos/id/234/300/200" />
+			<h3 class="event-time">Ongoing</h3>
+			<h2 class="event-title">JavaScript 101</h2>
+			<p class="event-location">Kitsilano</p>
+		</div>
+
+	</div>
+
+	<button>more</button>
+
+</div>
