4a9e014fb19ad4d7a5c02c4ee625d9e5b58732f3
Sina <sina202@gmail.com>
2019-12-06 15:36:44 -0800

    add partners styling plus  testimonials

--- themes/utg-2019/template-parts/about-page-testimonials.php
+++ themes/utg-2019/template-parts/about-page-testimonials.php
@@ -22,7 +22,8 @@
         ?>
         <?php if ( $the_testimonials->have_posts() ) : while ( $the_testimonials->have_posts() ) : $the_testimonials->the_post(); ?>
             <div class="testemonial-box">
-                <div class="testimonial-photo"><img src="<?php the_field('picture');?>"></div>
+                <div class="testimonial-photo" style="background: url(<?php echo the_field('picture'); ?>); background-size: cover; background-position: center"; ></div>
+
                 <div class="testimonial-content"><?php the_field('content');?></div>
                 <h3 class="testimonial-name"><?php the_title() ;?></h3>
                 <div class="testimonial-title"><?php the_field('title');?></div>
