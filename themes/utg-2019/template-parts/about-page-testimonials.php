d340afc48e511f5e5d376ce93557213c9748e7e2
Sina <sina202@gmail.com>
2019-12-06 15:41:04 -0800

    merge

--- themes/utg-2019/template-parts/about-page-testimonials.php
+++ themes/utg-2019/template-parts/about-page-testimonials.php
@@ -23,18 +23,10 @@
     ?>
     <?php if ($the_testimonials->have_posts()) : while ($the_testimonials->have_posts()) : $the_testimonials->the_post(); ?>
             <div class="testemonial-box">
-<<<<<<< HEAD
-                <div class="testimonial-photo" style="background: url(<?php echo the_field('picture'); ?>); background-size: cover; background-position: center"; ></div>
-
-                <div class="testimonial-content"><?php the_field('content');?></div>
-                <h3 class="testimonial-name"><?php the_title() ;?></h3>
-                <div class="testimonial-title"><?php the_field('title');?></div>
-=======
                 <div class="testimonial-photo" style=" background: url(<?php the_field('picture'); ?>); background-size:cover; background-position: center;"></div>
                 <div class="testimonial-content"><?php the_field('content'); ?></div>
                 <h3 class="testimonial-name"><?php the_title(); ?></h3>
                 <div class="testimonial-title"><?php the_field('title'); ?></div>
->>>>>>> e8b2d1c17e814b837307e137e33361d66614d9c8
             </div>
         <?php endwhile;
         else : ?> <p>Sorry, there are no posts to display</p> <?php endif; ?>
