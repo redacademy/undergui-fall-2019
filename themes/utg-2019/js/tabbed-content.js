2bb1a35805a03dcff41451090bfa156fb6593911
Sina <sina202@gmail.com>
2019-12-06 09:51:55 -0800

    tabbed content bug fix

--- themes/utg-2019/js/tabbed-content.js
+++ themes/utg-2019/js/tabbed-content.js
@@ -1,12 +1,11 @@
 document.addEventListener('DOMContentLoaded', function() {
-
+ 
 function onTabClick(event) {
     let activeTabs = document.querySelectorAll('.active');
     
     // deactivate existing active tab and panel    
     activeTabs.forEach(function(tab) {
-      tab.className = tab.className.replace('active', '');
-    //   if(event.target.tagName === "ul") return false;
+      tab.className = tab.className.replace('active', ''); 
     });
     
     // activate new tab and panel
