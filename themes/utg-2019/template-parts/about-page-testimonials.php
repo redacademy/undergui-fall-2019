439a72c08bdee8acf7ee6a0f525b9edf939a1dda
Sina <sina202@gmail.com>
2019-12-08 23:15:05 -0800

    improve styling on about testimonial, partners and story

--- themes/utg-2019/template-parts/about-page-testimonials.php
+++ themes/utg-2019/template-parts/about-page-testimonials.php
@@ -22,8 +22,8 @@
     $the_testimonials = new WP_Query($args);
     ?>
     <?php if ($the_testimonials->have_posts()) : while ($the_testimonials->have_posts()) : $the_testimonials->the_post(); ?>
-            <div class="testemonial-box">
-                <div class="testimonial-photo" style=" background: url(<?php the_field('picture'); ?>); background-size:cover; background-position: center;"></div>
+            <div class="testimonial-box">
+                <div class="testimonial-photo" style="background: url(<?php the_field('picture'); ?>); background-size:cover; background-position: center;"></div>
                 <div class="testimonial-content"><?php the_field('content'); ?></div>
                 <h3 class="testimonial-name"><?php the_title(); ?></h3>
                 <div class="testimonial-title"><?php the_field('title'); ?></div>
