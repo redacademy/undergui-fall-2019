3a2014b7abde3d572c766388eb236e77a665c968
Kieran Devlin <kjrdevlin@gmail.com>
2019-12-09 09:56:25 -0800

    restructure community post cards, add styles

--- themes/utg-2019/template-parts/community-page-posts.php
+++ themes/utg-2019/template-parts/community-page-posts.php
@@ -10,29 +10,35 @@
     </div>
 
     <!-- get post loop for 8 posts to be displayed on cummunity and get more to show up with more button -->
-    <?php
-    global $post;
+    <div class="post-container">
+        <?php
+        global $post;
 
-    $myposts = get_posts(array(
-        'posts_per_page' => -1,
-        'hide_empty'     => 1,
-    ));
+        $myposts = get_posts(array(
+            'posts_per_page' => -1,
+            'hide_empty'     => 1,
+        ));
 
-    if ($myposts) {
-        foreach ($myposts as $post) :
-            setup_postdata($post); ?>
-            <a class="site-card" href="<?php echo get_post_permalink(); ?>">
-                <div>
-                    <?php the_post_thumbnail('large'); ?>
-                    <?php echo get_the_date('F j, Y'); ?>
-                    <?php echo get_post_field('Catagory'); ?>
-                    <?php single_post_title(); ?>
-                </div>
-            </a>
-    <?php
-        endforeach;
-        wp_reset_postdata();
-    }
-    ?>
+        if ($myposts) {
+            foreach ($myposts as $post) :
+                setup_postdata($post); ?>
+                <a class="site-card" href="<?php echo get_post_permalink(); ?>">
+                    <?php the_post_thumbnail(); ?>
+                    <div class="post-meta">
+                        <div>
+                            <h3 class="post-date"><?php echo get_the_date('F j, Y'); ?></h3>
+                            <h2 class="post-title"><?= get_the_title(); ?></h2>
+
+                        </div>
+                        <p class="post-category"><?= get_sub_field($category) ?></p>
+                    </div>
+                </a>
+        <?php
+            endforeach;
+            wp_reset_postdata();
+        }
+        ?>
+
+    </div>
 
 </section>
