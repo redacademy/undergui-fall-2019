c010c00300c8f64d04cdd0feeccff8c491b2bd3f
Manav Singh <manavjot13@gmail.com>
2019-12-09 14:34:24 -0800

    added single-post.php

---
+++ themes/utg-2019/single-post.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * The template for displaying all single posts.
+ *
+ * @package utg_Theme
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+
+		
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php get_footer(); ?>
