669168cb2f8c6061e3b320e8689004976fe0d640
Kieran Devlin <kjrdevlin@gmail.com>
2019-12-10 09:14:42 -0800

    update posts page html, refactor events php and restructure html, add events scss partial, update site content padding, update events and posts styles

--- themes/utg-2019/template-parts/community-page-events.php
+++ themes/utg-2019/template-parts/community-page-events.php
@@ -1,7 +1,7 @@
 <section class="community-event-container">
 
     <h2>Events and important dates you don’t want to miss!.</h2>
-    
+
 
     <!-- get post loop for 8 posts to be displayed on cummunity and get more to show up with more button -->
     <?php
@@ -12,42 +12,42 @@
     );
     $the_events = new WP_Query($args);
     ?>
-    <?php if ($the_events->have_posts()) : while ($the_events->have_posts()) : $the_events->the_post(); ?>
-        <div class="eventg-box">
-            <h3 class="event-name"><?php the_title(); ?></h3>
 
-            <?php
+    <div class="post-container">
 
-            $rows = get_field('images' ); // get all the rows
-            $first_row = $rows[0]; // get the first row
-            $first_row_image = $first_row['image']; // get the sub field value 
+        <?php if ($the_events->have_posts()) : while ($the_events->have_posts()) : $the_events->the_post(); ?>
+                <a class="site-card">
 
-            ?>
+                    <!-- get the image for the event post -->
+                    <?php $eventImage = get_field('images')[0]['image']; // get the event post image 
+                            ?>
+                    <div class="image-container" style="background:url(<?= $eventImage; ?>); background-position: center; background-size:cover;">
+                    </div>
 
-            <img src="<?php echo $first_row_image; ?>" />
+                    <div class="post-meta">
+                        <div>
+                            <!-- check if there's an end date and appends date -->
+                            <p class="post-data"><?php (get_field('end_date')) ? the_field("starting_date") && the_field("end_date") : the_field("starting_date"); ?></p>
+                            <h3 class="post-title"><?php the_title(); ?></h3>
 
-            <!-- getting the location name for the loop -->
-            <?php
+                        </div>
 
-            $rows_location = get_field('location' ); // get all the rows
-            $first_row_location = $rows_location[0]; // get the first row
-            $first_row_location_name = $first_row_location['location_name']; // get the sub field value 
+                        <!-- get the location for the event post -->
+                        <p class="post-data"><?= $rows_location = get_field('location')[0]['location_name'];
+                                                        ?></p>
 
-            echo $first_row_location_name;
-            ?>
+                    </div>
 
 
-            <?php if( get_field('starting_date') ): ?>
-					<?php the_field("starting_date"); ?>
-				<?php endif; ?>
 
-                <?php if( get_field('end_date') ): ?>
-					 to <?php the_field("end_date"); ?> 
-				<?php endif; ?>
-    
-    
-        </div>
-    <?php endwhile;
-        else : ?> <p>Sorry, there are no posts to display</p> <?php endif; ?>
+
+
+
+                </a>
+            <?php endwhile; ?>
+
+    </div>
+<?php
+else : ?> <p>Sorry, there are no posts to display</p> <?php endif; ?>
 
 </section>
