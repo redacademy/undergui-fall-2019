a655f223bd79447217ee04f3644d1438281ead8d
Kieran Devlin <kjrdevlin@gmail.com>
2019-12-10 17:50:18 -0800

    troubleshoot tabbed content dploy issue

--- themes/utg-2019/js/tabbed-content.js
+++ themes/utg-2019/js/tabbed-content.js
@@ -3,12 +3,14 @@ document.addEventListener('DOMContentLoaded', function() {
     const element = document.getElementById('nav-tab');
     element.addEventListener('click', onTabClick, false);
   } else {
+    console.log(window.location.pathname);
     return;
   }
 
   function onTabClick(event) {
     let activeTabs = document.querySelectorAll('.active');
 
+    // console.log(event.target.href.split('#')[1]);
     // deactivate existing active tab and panel
     activeTabs.forEach(function(tab) {
       if (event.target.className === 'nav') {
