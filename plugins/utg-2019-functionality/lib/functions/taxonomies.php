c5aaf1c3ebcb0c7f3b8f3d74c54c692135853e06
Manav Singh <manavjot13@gmail.com>
2019-12-11 14:32:46 -0800

    added levels and age taxonomies to classes

--- plugins/utg-2019-functionality/lib/functions/taxonomies.php
+++ plugins/utg-2019-functionality/lib/functions/taxonomies.php
@@ -43,3 +43,81 @@ function create_programtaxonomy_tax() {
 
 }
 add_action( 'init', 'create_programtaxonomy_tax' );
+
+// Register Custom Taxonomy Level for classes
+function custom_levels_taxonomy() {
+
+	$labels = array(
+		'name'                       => _x( 'Levels', 'Taxonomy General Name', 'text_domain' ),
+		'singular_name'              => _x( 'Level', 'Taxonomy Singular Name', 'text_domain' ),
+		'menu_name'                  => __( 'Level Taxonomy', 'text_domain' ),
+		'all_items'                  => __( 'All Items', 'text_domain' ),
+		'parent_item'                => __( 'Parent Item', 'text_domain' ),
+		'parent_item_colon'          => __( 'Parent Item:', 'text_domain' ),
+		'new_item_name'              => __( 'New Item Name', 'text_domain' ),
+		'add_new_item'               => __( 'Add New Item', 'text_domain' ),
+		'edit_item'                  => __( 'Edit Item', 'text_domain' ),
+		'update_item'                => __( 'Update Item', 'text_domain' ),
+		'view_item'                  => __( 'View Item', 'text_domain' ),
+		'separate_items_with_commas' => __( 'Separate items with commas', 'text_domain' ),
+		'add_or_remove_items'        => __( 'Add or remove items', 'text_domain' ),
+		'choose_from_most_used'      => __( 'Choose from the most used', 'text_domain' ),
+		'popular_items'              => __( 'Popular Items', 'text_domain' ),
+		'search_items'               => __( 'Search Items', 'text_domain' ),
+		'not_found'                  => __( 'Not Found', 'text_domain' ),
+		'no_terms'                   => __( 'No items', 'text_domain' ),
+		'items_list'                 => __( 'Items list', 'text_domain' ),
+		'items_list_navigation'      => __( 'Items list navigation', 'text_domain' ),
+	);
+	$args = array(
+		'labels'                     => $labels,
+		'hierarchical'               => true,
+		'public'                     => true,
+		'show_ui'                    => true,
+		'show_admin_column'          => true,
+		'show_in_nav_menus'          => true,
+		'show_tagcloud'              => true,
+	);
+	register_taxonomy( 'level', array( 'post_classes' ), $args );
+
+}
+add_action( 'init', 'custom_levels_taxonomy', 0 );
+
+// Register Custom Taxonomy age for classes
+function custom_age_taxonomy() {
+
+	$labels = array(
+		'name'                       => _x( 'Ages', 'Taxonomy General Name', 'text_domain' ),
+		'singular_name'              => _x( 'Age', 'Taxonomy Singular Name', 'text_domain' ),
+		'menu_name'                  => __( 'Age Taxonomy', 'text_domain' ),
+		'all_items'                  => __( 'All Items', 'text_domain' ),
+		'parent_item'                => __( 'Parent Item', 'text_domain' ),
+		'parent_item_colon'          => __( 'Parent Item:', 'text_domain' ),
+		'new_item_name'              => __( 'New Item Name', 'text_domain' ),
+		'add_new_item'               => __( 'Add New Item', 'text_domain' ),
+		'edit_item'                  => __( 'Edit Item', 'text_domain' ),
+		'update_item'                => __( 'Update Item', 'text_domain' ),
+		'view_item'                  => __( 'View Item', 'text_domain' ),
+		'separate_items_with_commas' => __( 'Separate items with commas', 'text_domain' ),
+		'add_or_remove_items'        => __( 'Add or remove items', 'text_domain' ),
+		'choose_from_most_used'      => __( 'Choose from the most used', 'text_domain' ),
+		'popular_items'              => __( 'Popular Items', 'text_domain' ),
+		'search_items'               => __( 'Search Items', 'text_domain' ),
+		'not_found'                  => __( 'Not Found', 'text_domain' ),
+		'no_terms'                   => __( 'No items', 'text_domain' ),
+		'items_list'                 => __( 'Items list', 'text_domain' ),
+		'items_list_navigation'      => __( 'Items list navigation', 'text_domain' ),
+	);
+	$args = array(
+		'labels'                     => $labels,
+		'hierarchical'               => true,
+		'public'                     => true,
+		'show_ui'                    => true,
+		'show_admin_column'          => true,
+		'show_in_nav_menus'          => true,
+		'show_tagcloud'              => true,
+	);
+	register_taxonomy( 'age', array( 'post_classes' ), $args );
+
+}
+add_action( 'init', 'custom_age_taxonomy', 0 );
