4a25304ed4f8e7e4978e2cfbf87678f2224d5eb4
Kieran Devlin <kjrdevlin@gmail.com>
2019-12-10 17:25:54 -0800

    merge compile

--- themes/utg-2019/js/tabbed-content.js
+++ themes/utg-2019/js/tabbed-content.js
@@ -1,10 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
-
   if (window.location.pathname === '/utg/about/') {
-
     const element = document.getElementById('nav-tab');
     element.addEventListener('click', onTabClick, false);
-  
   } else {
     return;
   }
@@ -14,12 +11,15 @@ document.addEventListener('DOMContentLoaded', function() {
 
     // deactivate existing active tab and panel
     activeTabs.forEach(function(tab) {
-      if(event.target.className === 'nav') {return false}
-      tab.className = tab.className.replace('active', '');      
+      if (event.target.className === 'nav') {
+        return false;
+      }
+      tab.className = tab.className.replace('active', '');
     });
 
     // activate new tab and panel
     event.target.parentElement.className += ' active';
-    document.getElementById(event.target.href.split('#')[1]).className += ' active';
+    document.getElementById(event.target.href.split('#')[1]).className +=
+      ' active';
   }
 });
