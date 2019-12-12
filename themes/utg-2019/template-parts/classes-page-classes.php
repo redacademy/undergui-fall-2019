c49db9104a3a256796e280dd83f895c23a0b252f
Sina <sina202@gmail.com>
2019-12-12 13:00:05 -0800

    add class boxes for class page

--- themes/utg-2019/template-parts/classes-page-classes.php
+++ themes/utg-2019/template-parts/classes-page-classes.php
@@ -14,7 +14,7 @@
 			<a class="classes-card" href="<?php echo get_post_permalink(); ?>" style="background: <?= the_field('background_color'); ?> ">
 
 				<!-- dynamic post image for card -->
-				<div class="image-container" style="background:  url('<?php the_post_thumbnail_url('full'); ?>'), <?= the_field('background_color'); ?>  ; background-size: cover; background-position: center;">
+				<div class="class-container" style="background:  url('<?php the_post_thumbnail_url('full'); ?>'), <?= the_field('background_color'); ?>  ; background-size: cover; background-position: center;">
                 </div>
 
                 <!-- dynamic post title, date, and category -->
@@ -25,7 +25,7 @@
 						<!-- <div></div> -->
 					</div>
 
-					<div class="program-subfields">
+					<div class="classes-subfields">
 						<p class="post-data">Ages &nbsp;<?= the_field('age') ?></p>
 						<p class="post-data">Prerequisites &nbsp;<?= the_field('prerequisites') ?></p>
 
