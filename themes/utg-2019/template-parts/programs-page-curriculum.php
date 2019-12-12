eab40e5c25831adda8052293147e726959b39835
Kieran Devlin <kjrdevlin@gmail.com>
2019-12-11 16:34:23 -0800

    fix loop bug with wp_reset_query(), restructure curriculum html, update variables for site styles, add curriculum styles, update card mixins to adhere to designs, update hero banner styles

--- themes/utg-2019/template-parts/programs-page-curriculum.php
+++ themes/utg-2019/template-parts/programs-page-curriculum.php
@@ -1,14 +1,14 @@
 <article class="program-curriculum">
 
     <img src="<?php the_field('bottom_page_image'); ?>" alt="">
-    <div class="curriculum">
-        <h2><?php the_field('curriculum_title'); ?></h2>
-        <?php the_field('curriculum_paragraph'); ?>
 
+    <div class="curriculum">
+        <h4><?php the_field('curriculum_title'); ?></h4>
+        <?php the_field('curriculum_paragraph'); ?>
     </div>
 
     <div class="additional-info">
-        <h2><?php the_field('additional_info_title'); ?></h2>
+        <h4><?php the_field('additional_info_title'); ?></h4>
         <?php the_field('additional_info_paragraph'); ?>
     </div>
 
