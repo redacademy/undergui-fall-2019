a1bf103f5a7acf7a7bb007f4279de9f8c7231360
Sina <sina202@gmail.com>
2019-12-13 15:46:53 -0800

    mobile menu & animation

--- themes/utg-2019/js/mobile-menu.js
+++ themes/utg-2019/js/mobile-menu.js
@@ -1,23 +1,21 @@
 document.addEventListener('DOMContentLoaded', function() {
   const hamburgerMenu = document.querySelector('.mobile-menu');
   const navLinks = document.querySelector('.nav-links');
-  // const siteHeader = document.querySelector('.site-header');
+  // const mainMenu = document.querySelector('#nav-links')
+  const siteHeader = document.querySelector('.site-header');
   const closeMenuButton = document.querySelector('.left-arrow');
 
   //on click hamburger icon, opens mobile menu
   hamburgerMenu.addEventListener('click', function(e) {
     e.preventDefault();
-    // closeMenuButton.style.transform = 'rotate(180deg)';
-    // closeMenuButton.classList.remove('close-left-arrow');
-    // siteHeader.classList.add('toggle-menu');
+    siteHeader.classList.add('toggle-menu');
     navLinks.classList.add('toggle-menu');
   });
 
   //on click arrow button, switches arrow direction and closes menu
   closeMenuButton.addEventListener('click', function(e) {
     e.preventDefault();
-    // closeMenuButton.style.transform = 'rotate(-180deg)';
-    // siteHeader.classList.remove('toggle-menu');
+    siteHeader.classList.remove('toggle-menu');
     navLinks.classList.remove('toggle-menu');
   });
 });
