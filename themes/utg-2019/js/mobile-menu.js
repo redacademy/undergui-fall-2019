9fa5b0008bce98d39203fabd2d22ef5f570c549c
Sina <sina202@gmail.com>
2019-12-13 13:57:52 -0800

    add fancy animation to mobile menu slide and closing arrow

--- themes/utg-2019/js/mobile-menu.js
+++ themes/utg-2019/js/mobile-menu.js
@@ -1,28 +1,23 @@
 document.addEventListener('DOMContentLoaded', function() {
   const hamburgerMenu = document.querySelector('.mobile-menu');
   const navLinks = document.querySelector('.nav-links');
-  const siteHeader = document.querySelector('.site-header');
+  // const siteHeader = document.querySelector('.site-header');
   const closeMenuButton = document.querySelector('.left-arrow');
-  // const switchArrowDireciton = document.querySelector('.right-arrow');
-  // const hoverDropMenu = document.querySelector('.nav-menu a');
 
   //on click hamburger icon, opens mobile menu
   hamburgerMenu.addEventListener('click', function(e) {
     e.preventDefault();
-    closeMenuButton.style.transform = 'rotate(180deg)';
-    closeMenuButton.classList.remove('close-left-arrow');
-    // switchArrowDireciton.classList.remove('open-right-arrow');
-    siteHeader.classList.add('toggle-menu');
+    // closeMenuButton.style.transform = 'rotate(180deg)';
+    // closeMenuButton.classList.remove('close-left-arrow');
+    // siteHeader.classList.add('toggle-menu');
     navLinks.classList.add('toggle-menu');
   });
 
   //on click arrow button, switches arrow direction and closes menu
   closeMenuButton.addEventListener('click', function(e) {
     e.preventDefault();
-    // closeMenuButton.classList.add('close-left-arrow');
-    // switchArrowDireciton.classList.add('open-right-arrow');
-
-    siteHeader.classList.remove('toggle-menu');
+    // closeMenuButton.style.transform = 'rotate(-180deg)';
+    // siteHeader.classList.remove('toggle-menu');
     navLinks.classList.remove('toggle-menu');
   });
 });
