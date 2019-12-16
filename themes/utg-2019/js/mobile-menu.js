acf271e71911a21f4954aeac09baaaed14859240
Sina <sina202@gmail.com>
2019-12-16 14:21:16 -0800

    fix issue with mobile menu height

--- themes/utg-2019/js/mobile-menu.js
+++ themes/utg-2019/js/mobile-menu.js
@@ -3,12 +3,14 @@ document.addEventListener('DOMContentLoaded', function() {
   const navLinks = document.querySelector('.nav-links');
   const siteHeader = document.querySelector('.site-header');
   const closeMenuButton = document.querySelector('.left-arrow');
+  const blockMan = document.querySelector('.block-man-chat-mobile')
 
   //on click hamburger icon, opens mobile menu
   hamburgerMenu.addEventListener('click', function(e) {
     e.preventDefault();
     siteHeader.classList.add('toggle-menu');
     navLinks.classList.add('toggle-menu');
+    blockMan.classList.add('hide-block-man')
   });
 
   //on click arrow button, switches arrow direction and closes menu
@@ -16,5 +18,9 @@ document.addEventListener('DOMContentLoaded', function() {
     e.preventDefault();
     siteHeader.classList.remove('toggle-menu');
     navLinks.classList.remove('toggle-menu');
+    setTimeout(function() {
+      blockMan.classList.remove('hide-block-man')
+    }, 400)
+
   });
 });
