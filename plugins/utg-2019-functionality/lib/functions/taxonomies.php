25d3bf6ac5fab86769665a50a8ee637135f54192
Manav Singh <manavjot13@gmail.com>
2019-12-16 08:57:50 -0800

    added taxonomies to classes

--- plugins/utg-2019-functionality/lib/functions/taxonomies.php
+++ plugins/utg-2019-functionality/lib/functions/taxonomies.php
@@ -121,3 +121,81 @@ function custom_age_taxonomy() {
 
 }
 add_action( 'init', 'custom_age_taxonomy', 0 );
+
+// Register Custom Taxonomy age for classes
+function custom_day_taxonomy() {
+
+	$labels = array(
+		'name'                       => _x( 'Days', 'Taxonomy General Name', 'text_domain' ),
+		'singular_name'              => _x( 'Day', 'Taxonomy Singular Name', 'text_domain' ),
+		'menu_name'                  => __( 'Day Taxonomy', 'text_domain' ),
+		'all_items'                  => __( 'All Items', 'text_domain' ),
+		'parent_item'                => __( 'Parent Item', 'text_domain' ),
+		'parent_item_colon'          => __( 'Parent Item:', 'text_domain' ),
+		'new_item_name'              => __( 'New Item Name', 'text_domain' ),
+		'add_new_item'               => __( 'Add New Item', 'text_domain' ),
+		'edit_item'                  => __( 'Edit Item', 'text_domain' ),
+		'update_item'                => __( 'Update Item', 'text_domain' ),
+		'view_item'                  => __( 'View Item', 'text_domain' ),
+		'separate_items_with_commas' => __( 'Separate items with commas', 'text_domain' ),
+		'add_or_remove_items'        => __( 'Add or remove items', 'text_domain' ),
+		'choose_from_most_used'      => __( 'Choose from the most used', 'text_domain' ),
+		'popular_items'              => __( 'Popular Items', 'text_domain' ),
+		'search_items'               => __( 'Search Items', 'text_domain' ),
+		'not_found'                  => __( 'Not Found', 'text_domain' ),
+		'no_terms'                   => __( 'No items', 'text_domain' ),
+		'items_list'                 => __( 'Items list', 'text_domain' ),
+		'items_list_navigation'      => __( 'Items list navigation', 'text_domain' ),
+	);
+	$args = array(
+		'labels'                     => $labels,
+		'hierarchical'               => true,
+		'public'                     => true,
+		'show_ui'                    => true,
+		'show_admin_column'          => true,
+		'show_in_nav_menus'          => true,
+		'show_tagcloud'              => true,
+	);
+	register_taxonomy( 'day', array( 'post_classes' ), $args );
+
+}
+add_action( 'init', 'custom_time_taxonomy', 0 );
+
+// Register Custom Taxonomy age for classes
+function custom_age_taxonomy() {
+
+	$labels = array(
+		'name'                       => _x( 'Times', 'Taxonomy General Name', 'text_domain' ),
+		'singular_name'              => _x( 'Time', 'Taxonomy Singular Name', 'text_domain' ),
+		'menu_name'                  => __( 'Time Taxonomy', 'text_domain' ),
+		'all_items'                  => __( 'All Items', 'text_domain' ),
+		'parent_item'                => __( 'Parent Item', 'text_domain' ),
+		'parent_item_colon'          => __( 'Parent Item:', 'text_domain' ),
+		'new_item_name'              => __( 'New Item Name', 'text_domain' ),
+		'add_new_item'               => __( 'Add New Item', 'text_domain' ),
+		'edit_item'                  => __( 'Edit Item', 'text_domain' ),
+		'update_item'                => __( 'Update Item', 'text_domain' ),
+		'view_item'                  => __( 'View Item', 'text_domain' ),
+		'separate_items_with_commas' => __( 'Separate items with commas', 'text_domain' ),
+		'add_or_remove_items'        => __( 'Add or remove items', 'text_domain' ),
+		'choose_from_most_used'      => __( 'Choose from the most used', 'text_domain' ),
+		'popular_items'              => __( 'Popular Items', 'text_domain' ),
+		'search_items'               => __( 'Search Items', 'text_domain' ),
+		'not_found'                  => __( 'Not Found', 'text_domain' ),
+		'no_terms'                   => __( 'No items', 'text_domain' ),
+		'items_list'                 => __( 'Items list', 'text_domain' ),
+		'items_list_navigation'      => __( 'Items list navigation', 'text_domain' ),
+	);
+	$args = array(
+		'labels'                     => $labels,
+		'hierarchical'               => true,
+		'public'                     => true,
+		'show_ui'                    => true,
+		'show_admin_column'          => true,
+		'show_in_nav_menus'          => true,
+		'show_tagcloud'              => true,
+	);
+	register_taxonomy( 'time', array( 'post_classes' ), $args );
+
+}
+add_action( 'init', 'custom_time_taxonomy', 0 );
